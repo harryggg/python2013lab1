1d0d40df2f6f7d29589772bf1361c3bb55669d5d
harryggg <ma.tanghao@dhs.sg>
2013-01-20 02:47:34 +0800

    Update master_or_visa.py

--- master_or_visa.py
+++ master_or_visa.py
@@ -4,15 +4,17 @@
 def master_or_visa(n):
     #strip space and -
     cardnumber=n.replace(' ','').replace('-','') 
-    #check for invalid char
+    #check for invalid character
     try:
         x=int(cardnumber)
     except(ValueError):
         return 'Input Wrong. contains invalid character.'
     length = len(n)
     #categorize
+    #if length == 13,16 and first number is ‘4’ : visa, end
     if (length == 13 or length == 16) and (cardnumber[0]=='4'):
         return 'It is Visa'
+    #if length == 16 and first-second number is ‘51’-’55’: mastercard end
     if length == 16 and 51<=int(cardnumber[0:2])<=55:
         return 'It is MasterCard'
 
@@ -20,6 +22,6 @@ def master_or_visa(n):
     return 'It is neither'
 
 #main
-
+#input cardnumber
 card=input('plz input your card number?\n')
 print(master_or_visa(card))
